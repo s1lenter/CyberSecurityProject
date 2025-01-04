cdd574477577cf942812560ef653ef54f84198a9
s1lenter <sega29600@gmail.com>
2025-01-04 19:21:23 +0500

    Add static for admin page

--- CyberSecuritySite/CyberSecuritySite/settings.py
+++ CyberSecuritySite/CyberSecuritySite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 
 # Application definition
 
