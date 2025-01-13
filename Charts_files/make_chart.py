75c39c7bb27d600d6a76c54ae4c1936c74c95fc2
s1lenter <sega29600@gmail.com>
2025-01-13 20:22:55 +0500

    Написать создание графика для топа вакансий

--- Charts_files/make_chart.py
+++ Charts_files/make_chart.py
@@ -33,8 +33,26 @@ def make_pie(file, title, x_name, y_name):
     plt.title(title)
     plt.show()
 
+def make_top_column_chart(file, title, x_name, y_name):
+    df = pd.read_csv(file)
+
+    df[x_name] = pd.to_numeric(df[x_name])
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(df[x_name], df[y_name])
+    plt.title(title)
+    plt.xticks(df[x_name], rotation=90)
+    years = df[x_name]
+    skills = df['Навык']
+    count = df[y_name]
+    for i in range(len(years)):
+        plt.text(years[i], count[i], skills[i], ha='center', va='bottom')
+    plt.grid(axis='y')
+    plt.show()
+
 
 # make_column_chart('year_salary.csv', 'Динамика уровня зарплат по годам', 'Год', 'Средняя зарплата')
 # make_column_chart('year_count.csv', 'Динамика количества вакансий по годам', 'Год', 'Количество вакансий')
 # horizontal_chart('city_salary.csv', 'Уровень зарплат по городам', 'Город', 'Средняя зарплата')
-make_pie('city_part.csv', 'Доля вакансий по городам', 'Доля вакансий', 'Город')
+# make_pie('city_part.csv', 'Доля вакансий по городам', 'Доля вакансий', 'Город')
+#make_top_column_chart('top-20.csv', 'Топ навыков по годам', 'Год', 'Количество')
